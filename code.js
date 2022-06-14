09c48f34198e60efe338a1e76ed3700d81163f54
Reagan <kingreggie702@gmail.com>
2022-06-15 01:25:58 +0200

    Added changing button colors

--- code.js
+++ code.js
@@ -23,6 +23,7 @@ async function fetchData(){
     return pokemon
 }
 
+// Function that fetches all the pokémon types
 async function fetchTypes(){
     let result = await fetch('https://pokeapi.co/api/v2/type');
     let data = await result.json();
@@ -60,7 +61,7 @@ function CreatePokémonHTML(newPokemon,pokeInfo,index,currentIteration,typeData)
                     <ul class="list-unstyled">
                             <li> Weight: ${pokeInfo[currentIteration].weight} </li>
                             <li> Height: ${pokeInfo[currentIteration].height} </li>
-                            <li> Type: <button class="btn" id="" >${pokeInfo[currentIteration].types[0].type.name.charAt(0).toUpperCase() + pokeInfo[currentIteration].types[0].type.name.substring(1)} </button> </li>
+                            <li> Type: <button style="background-color:${chooseColor(pokeInfo[currentIteration].types[0].type.name,typeData)} " class="btn" id="" >${pokeInfo[currentIteration].types[0].type.name.charAt(0).toUpperCase() + pokeInfo[currentIteration].types[0].type.name.substring(1)} </button> </li>
                         </ul>
                         <p class="card-text">${newPokemon[currentIteration].flavor_text_entries[1].flavor_text}</p>
                     </div>
@@ -68,38 +69,59 @@ function CreatePokémonHTML(newPokemon,pokeInfo,index,currentIteration,typeData)
             </div>`;
 }
 
-function convertTo(ar){
-    let object = {};
-    for(let i = 0; i<ar.length;i++){
-        let key = ar[0][i];
-        let value = ar[1][i];
-        object[key] = value;
-    }
-    return object;
-}
-
+// Converts the type and color arrays into a 2 Dimensional array.
 function convertTo2D(array1,array2){
     let i = -1;
     let result = [];
     while(array1[++i]){
         result.push([array1[i],array2[i]])
     }
+    return convertTo(result);
+}
+
+// Then converts the 2D Array into an object
+
+function convertTo(ar){
+    let object = {};
+    for(let i = 0; i<ar.length;i++){
+        let key = ar[i][0];
+        let value = ar[i][1];
+        object[key] = value;
+    }
+    return object;
+}
+
+// Checks the corresponding value to the corresponding key in the object and returns a color
+
+function chooseColor(pokeType,typeData){
+    let result;
+    if(typeData != undefined && typeData.hasOwnProperty(pokeType)){
+        result = (typeData[pokeType]);
+    }else if(pokeType == 'bug'){
+        result = 'darkolivegreen';
+    }
     return result;
 }
 
-// let types = fetchTypes();
-// let colors = ["darkolivegreen",'brown','royalblue', 'yellow', 'pink', 'darkred','red','lightseagreen','darkblue', 'green', 'goldenrod', 'cyan', 'silver','purple','magenta','gray','blue'
-// ];
+// Initializes typeData object
 
-// let conversion = convertTo2D(types,colors);
-// let newArray = convertTo(conversion);
+async function manageTypes(){
+    // Fetches the data from the API
+    let types = await fetchTypes();
+    // Creates an array of all possible colors
+    let colors = ['silver', 'darkred','lightseagreen','purple', 'goldenrod','goldenrod','darkolivegreen', 'darkblue','gray','red','blue','green','yellow','magenta','cyan','royalblue', 'brown','pink', 'gray', 'darkmagenta'
+    ];
+    // Converts the two arrays into an object
+    let newObject = convertTo2D(types,colors);
+    return newObject;
+}
 
 // Function that creates rows and displays information in cards, in columns, in the rows
 async function display(){
     // Calls the functions the already fetched information
-    let typeData = await fetchTypes()
     let newPokemon = await fetchData();
     let pokeInfo = await fetchPokéData();
+    let typeData = await manageTypes();
 
     // Loop that creates rows per 3 pokemon requested, starting from zero. Math.ceil() returns the next highest integer
     for(let i = 0; i < Math.ceil(newPokemon.length/3); i++){
