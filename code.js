45e79c9be616002229389fa164b06a3351a15b07
Reagan701 <kingreggie702@gmail.com>
2022-07-21 11:36:37 +0200

    Code changes

--- code.js
+++ code.js
@@ -2,7 +2,7 @@
 let container = document.getElementById("mainContainer");
 
 // Number of pokémon fetched from the API
-let numberOfFetchedPokémon = 30;
+let numberOfFetchedPokémon = 60;
 
 // Function to fetch pokédex information
 async function fetchData(){
@@ -50,10 +50,9 @@ async function fetchPokéData(){
     return pokemon;
 }
 
-function CreatePokémonHTML(newPokemon,pokeInfo,index,currentIteration,typeData){
-    container = document.getElementById(index);
+function CreatePokémonHTML(newPokemon,pokeInfo,currentIteration,typeData){
             container.innerHTML +=
-            `<div class="col-md-4">
+            `<div class="col-md-4 mt-4">
                 <div id="border" class="card w-100">
                     <img id="cardImage" class="img-fluid" src="${pokeInfo[currentIteration].sprites.front_default}" alt="">
                     <div id="cardBody" class="text-white card-body">
@@ -88,6 +87,7 @@ function convertTo(ar){
         let value = ar[i][1];
         object[key] = value;
     }
+    console.log(object)
     return object;
 }
 
@@ -118,64 +118,13 @@ async function manageTypes(){
 
 // Function that creates rows and displays information in cards, in columns, in the rows
 async function display(){
+    
     // Calls the functions the already fetched information
     let newPokemon = await fetchData();
     let pokeInfo = await fetchPokéData();
     let typeData = await manageTypes();
-
-    // Loop that creates rows per 3 pokemon requested, starting from zero. Math.ceil() returns the next highest integer
-    for(let i = 0; i < Math.ceil(newPokemon.length/3); i++){
-        // Creates div and applies the appropriate ID and classes
-        let newRow = document.createElement('div');
-        newRow.className = "row my-5";
-        newRow.id = i;
-        container.appendChild(newRow);
-    }
-    let currentIteration;
-
-    for(let i = 0; i < newPokemon.length;i++){
-        switch(true){
-            case (i<3):
-                currentIteration =i;
-                CreatePokémonHTML(newPokemon,pokeInfo,0,currentIteration,typeData);
-            break;
-            case(i>=3 && i<6):
-                currentIteration = i;
-                CreatePokémonHTML(newPokemon,pokeInfo,1,currentIteration,typeData);
-            break;
-            case(i>=6 && i<9):
-            currentIteration = i;
-                CreatePokémonHTML(newPokemon,pokeInfo,2,currentIteration,typeData);
-            break;
-            case(i>=9 && i<12):
-                currentIteration = i;
-                CreatePokémonHTML(newPokemon,pokeInfo,3,currentIteration),typeData;
-            break;
-            case(i>=12 && i<15):
-            currentIteration = i;
-                CreatePokémonHTML(newPokemon,pokeInfo,4,currentIteration,typeData);
-            break;
-            case(i>=15 && i<18):
-            currentIteration = i;
-                CreatePokémonHTML(newPokemon,pokeInfo,5,currentIteration,typeData);
-            break;
-            case(i>=18 && i<21):
-            currentIteration = i;
-                CreatePokémonHTML(newPokemon,pokeInfo,6,currentIteration,typeData);
-            break;
-            case(i>=21 && i<24):
-            currentIteration = i;
-                CreatePokémonHTML(newPokemon,pokeInfo,7,currentIteration,typeData);
-            break;
-            case(i>=24 && i<27):
-            currentIteration = i;
-                CreatePokémonHTML(newPokemon,pokeInfo,8,currentIteration,typeData);
-            break;
-            case(i>=27 && i<30):
-            currentIteration = i;
-                CreatePokémonHTML(newPokemon,pokeInfo,9,currentIteration,typeData);
-            break;
-        }
+    for(let i = 0; i<newPokemon.length;i++){
+        CreatePokémonHTML(newPokemon,pokeInfo,i,typeData);
     }
 }
 
